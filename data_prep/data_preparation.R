d5bbcbce548a49a6bb446ea6c98255f22d279fa3
Corey Dow-Hygelund <cdowhygelund@mozilla.com>
2019-02-17 09:42:38 -0800

    Modifying directory location.

--- data_prep/data_preparation.R
+++ data_prep/data_preparation.R
@@ -3,11 +3,11 @@
 
 # Parse the client mean histograms into a single data frame
 source('data_prep_helpers.R')
-client_means <- build_client_means.df(list.files('data/client_means/', pattern = 'part*', 
+client_means <- build_client_means.df(list.files('../data/client_means/', pattern = 'part*', 
                                                  full.names = TRUE))
 # Add a Date Field
 library(lubridate)
 client_means$date <- as_date(as.character(client_means$app_build_id))
 
 # Save off for later consumption
-save(client_means, file = 'data/live/client_means.RData')
+save(client_means, file = '../data/live/client_means.RData')
